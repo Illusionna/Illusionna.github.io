8aaa718ee68e510f7c1a219b32f348e5ff434454
Zolio Marling <109838848+Illusionna@users.noreply.github.com>
2024-05-13 13:23:58 +0800

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+function showSubmenu(id) {
+    var submenu = document.getElementById(id + '-submenu');
+    submenu.classList.toggle('show');
+}
+function showSubsubmenu(id) {
+    var subsubmenu = document.getElementById(id + '-subsubmenu');
+    subsubmenu.classList.toggle('show');
+}
