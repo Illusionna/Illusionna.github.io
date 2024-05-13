4bea90e287fa66fe31151499689761e3f8292bca
Zolio Marling <109838848+Illusionna@users.noreply.github.com>
2024-05-14 01:10:13 +0800

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,3 +6,7 @@ function showSubsubmenu(id) {
     var subsubmenu = document.getElementById(id + '-subsubmenu');
     subsubmenu.classList.toggle('show');
 }
+function showSubsubsubmenu(id) {
+    var subsubsubmenu = document.getElementById(id + '-subsubsubmenu');
+    subsubsubmenu.classList.toggle('show');
+}
