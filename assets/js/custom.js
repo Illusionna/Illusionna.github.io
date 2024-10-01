76108c67c3595db4d80c50b80934447a99849d5f
Zolio Marling <109838848+Illusionna@users.noreply.github.com>
2024-10-01 21:44:38 +0800

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+function show2menu(id) {
+    var menu = document.getElementById(id + '-sub2menu');
+    menu.classList.toggle('show');
+}
+function show3menu(id) {
+    var menu = document.getElementById(id + '-sub3menu');
+    menu.classList.toggle('show');
+}
+function show4menu(id) {
+    var menu = document.getElementById(id + '-sub4menu');
+    menu.classList.toggle('show');
+}
+function show5menu(id) {
+    var menu = document.getElementById(id + '-sub5menu');
+    menu.classList.toggle('show');
+}
+function show6menu(id) {
+    var menu = document.getElementById(id + '-sub6menu');
+    menu.classList.toggle('show');
+}
+function show7menu(id) {
+    var menu = document.getElementById(id + '-sub7menu');
+    menu.classList.toggle('show');
+}
